7c387968bae3c136a3a70636924312ce797c2185
timhatton <tim@git.hub>
2021-05-23 20:38:49 +0100

    Initial commit

---
+++ HueSundowner/HueController.cs
@@ -0,0 +1,49 @@
+﻿using Q42.HueApi;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HueSundowner.Lib {
+  public class HueController {
+    public HueController(string ipAddress, string appKey) {
+      this.ipAddress = ipAddress;
+      this.appKey = appKey;
+    }
+    public async Task On() {
+      try {
+        logger.Information("Turning lights on");
+        LocalHueClient client = new LocalHueClient(ipAddress);
+        client.Initialize(appKey);
+        var command = new LightCommand();
+        command.On = true;        
+
+        await client.SendCommandAsync(command);
+      }
+      catch(Exception ex) {
+        logger.Information("Turning lights on");
+        logger.Information($"Error turning lights on: {ex.Message}");
+      }
+    }
+    public async Task Off() {
+      try {
+        logger.Information("Turning lights off");
+        LocalHueClient client = new LocalHueClient(ipAddress);
+        client.Initialize(appKey);
+        var command = new LightCommand();
+        command.On = false;
+
+        await client.SendCommandAsync(command);
+      }
+      catch(Exception ex) {
+        logger.Information("Turning lights off");
+        logger.Information($"Error turning lights off: {ex.Message}");
+      }
+    }
+    string ipAddress;
+    string appKey;
+    private static readonly ILogger logger = Log.Logger.ForContext<HueController>();
+  }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using HueSundowner.Lib;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestConsole {
+  class Program {
+    static async Task Main(string[] args) {    
+      if(args.Length != 1 || (args[0].ToLower() != "on" && args[0].ToLower() != "off")) {
+        Console.WriteLine("test [on | off]");
+        Environment.Exit(1);
+      }
+
+      var builder = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json");
+
+      var configuration = builder.Build();
+
+      var settings = configuration.GetSection("HueSettings").Get<HueSettings>();
+
+      Console.WriteLine($"Hue bridge at {settings.IpAddress}");
+      var controller = new HueController(settings.IpAddress, settings.AppKey);
+
+      if(args[0].ToLower() == "on") {
+        await controller.On();
+      }
+      else if(args[0].ToLower() == "off") {
+        await controller.Off();
+      }
+    }
+  }
+}
