fa7dfc353c8dccefd69bdd8aaf425280e1083ade
timhatton <tim@git.hub>
2021-05-26 21:57:04 +0100

    Made logdirectory configurable

--- HueSundownerService/Program.cs
+++ HueSundownerService/Program.cs
@@ -2,21 +2,30 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using Serilog;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace HueSundownerService {
   public class Program {
     public static void Main(string[] args) {
-      Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Debug()
-        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
-        .Enrich.FromLogContext()
-        .WriteTo.File(@"c:\HueSundownerLogs\huesundowner.log")
-#if DEBUG
-        .WriteTo.Console()
-#endif
-        .CreateLogger();
 
       try {
+        var builder = new ConfigurationBuilder()
+         .SetBasePath(Directory.GetCurrentDirectory())
+         .AddJsonFile("appSettings.json");
+        var configuration = builder.Build();
+        var serilogSettings = configuration.GetSection("SerilogSettings").Get<SerilogSettings>();
+
+        Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .WriteTo.File(serilogSettings.LogFile)
+#if DEBUG
+                        .WriteTo.Console()
+#endif
+                        .CreateLogger();
+
         Log.Information("Starting service");
         CreateHostBuilder(args).Build().Run();
         return;
